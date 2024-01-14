586828b5dc1b8fe9799ae7145afda9865bd2c634
Cesar <cesarochoa2006@github.com>
2024-01-14 10:56:04 -0500

    test: adds module assertion

--- src/test/java/com/cesarochoa/modulith/ModulithSampleApplicationTests.java
+++ src/test/java/com/cesarochoa/modulith/ModulithSampleApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.modulith.core.ApplicationModules;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class ModulithSampleApplicationTests {
 
@@ -12,6 +14,7 @@ class ModulithSampleApplicationTests {
 	void createApplicationModuleModel() {
 		ApplicationModules modules = ApplicationModules.of(Application.class);
 		modules.forEach(System.out::println);
+		assertEquals(2, modules.stream().count());
 	}
 
 }
