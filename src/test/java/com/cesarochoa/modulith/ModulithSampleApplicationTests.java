63ff8295dc1cadffef80ae68db3b4d9155416c44
Cesar <cesarochoa2006@github.com>
2024-01-14 10:53:22 -0500

    chore: adds module print

--- src/test/java/com/cesarochoa/modulith/ModulithSampleApplicationTests.java
+++ src/test/java/com/cesarochoa/modulith/ModulithSampleApplicationTests.java
@@ -1,13 +1,17 @@
 package com.cesarochoa.modulith;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
 
 @SpringBootTest
 class ModulithSampleApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void createApplicationModuleModel() {
+		ApplicationModules modules = ApplicationModules.of(Application.class);
+		modules.forEach(System.out::println);
 	}
 
 }
